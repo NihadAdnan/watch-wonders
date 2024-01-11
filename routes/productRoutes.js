ba5029456815152de2ff2dd8eafb191c56205c63
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-01-11 22:13:13 +0600

    all the codes
    mern stack ecommerce app for watch

---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+import  express  from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import { braintreePaymentController, braintreeTokenController, categoryProductController, countProductController, createProductController, deleteProductController, filterProductController, getProductController, getSingleProductController, listProductController, photoProductController, relatedProductController, searchProductController, updateProductController } from "../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+router.post(
+    "/create-product",requireSignIn,isAdmin,formidable(),createProductController
+  );
+router.put(
+    "/update-product/:pid",requireSignIn,isAdmin,formidable(),updateProductController);
+router.get("/get-product",getProductController);
+router.get("/get-product/:slug",getSingleProductController);
+router.get("/photo-product/:pid",photoProductController);
+router.delete("/delete-product/:pid",deleteProductController);
+router.post("/filter-product",filterProductController);
+router.get("/count-product",countProductController);
+router.get("/list-product/:page",listProductController);
+router.get("/search/:keyword",searchProductController);
+router.get("/related-product/:pid/:cid",relatedProductController);
+router.get("/category-product/:slug",categoryProductController);
+router.get("/braintree/token",braintreeTokenController);
+router.post("/braintree/payment",requireSignIn,braintreePaymentController);
+
+export default router
